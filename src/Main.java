b47e4aa85fc1d8767aee47d357382d8055b58053
Irinaabieva <irinaabieva03@gmail.com>
2022-11-01 10:45:10 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        int age = 19;
+        if (age >= 18) {
+            System.out.println("Поздравляю вы совершеннолетны!");
+        }
+        else {
+            System.out.println("Возраст совершеннолетия еще не наступил");
+        }
     }
 }
